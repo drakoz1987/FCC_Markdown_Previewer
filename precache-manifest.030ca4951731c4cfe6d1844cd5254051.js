348950d1132dc2f59efee51e9d503140f23fd381
De Freitas <drakoz1987@gmail.com>
2019-05-16 06:50:07 -0400

    Updates

---
+++ precache-manifest.030ca4951731c4cfe6d1844cd5254051.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7223e8c46c398d09a71fa2c042c554",
+    "url": "/drakoz1987/FCC_Markdown_Previewer.git/index.html"
+  },
+  {
+    "revision": "165f71c8247387617cf6",
+    "url": "/drakoz1987/FCC_Markdown_Previewer.git/static/js/2.f69b136b.chunk.js"
+  },
+  {
+    "revision": "8de247423143dda00f42",
+    "url": "/drakoz1987/FCC_Markdown_Previewer.git/static/js/main.5065c2b3.chunk.js"
+  },
+  {
+    "revision": "af7ea76e3cb94f852108",
+    "url": "/drakoz1987/FCC_Markdown_Previewer.git/static/js/runtime~main.2cb4b1bd.js"
+  }
+]);
